1dacf55971196b3d7102880f41295dcb20dc5341
blee241 <128437170+blee241@users.noreply.github.com>
2023-05-22 18:00:34 -0700

    weather data now displays in the first day of the forecast

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-var lat;
-var lon;
 const apiKey = 'be54b35146f1d2f53bd09b1612c4abbf';
 const apiCallGeolocator = city => `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
-const apiCallCurrentWeather = (latitude, longitude) => `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-const apiCallFiveDay = (latitude, longitude) => `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apikey}`
+const apiCallCurrentWeather = (latitude, longitude) => `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+const apiCallFiveDay = (latitude, longitude) => `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
 const searchBtnEl = document.getElementById('searchBtn');
 
 //Variables for the currect city elements
@@ -11,6 +9,10 @@ const currentCityEl = document.getElementById('currentCity');
 const currentTempEl = document.getElementById('currentTemp');
 const currentWindEl = document.getElementById('currentWind');
 const currentHumidityEl = document.getElementById('currentHumidity');
+
+const dayOneTempEl = document.getElementById('dayOneTemp');
+const dayOneWindEl = document.getElementById('dayOneWind');
+const dayOneHumidityEl = document.getElementById('dayOneHumidity');
 
 //Grabs the latitude and longitude of a searched city
 const getLatLon = (city) => {
@@ -20,15 +22,15 @@ const getLatLon = (city) => {
         })
         .then((data) => {
             //Saves lat and lon coordinates data to corresponding vars
-            lat = data[0].lat;
-            lon = data[0].lon;
+            let lat = data[0].lat;
+            let lon = data[0].lon;
             lat = Math.round(lat * 100) / 100;
             lon = Math.round(lon * 100) / 100;
             //Resolves the promise by outputting lat and lon in an array
             return([lat, lon])
         })
         .catch(err => console.log(err));
-}
+};
 
 const fetchCurrentWeather = ([latitude, longitude]) => {
     //This makes an api call for the weather using the lat and lon from the previous promise
@@ -41,25 +43,48 @@ const fetchCurrentWeather = ([latitude, longitude]) => {
             return data;
         })
         .catch(err => console.log(err));
-}
+};
+
+const fetchFiveDayWeather = ([latitude, longitude]) => {
+    return fetch(apiCallFiveDay(latitude, longitude))
+        .then((res) => {
+            return res.json();
+        })
+        .then(data => {
+            return data;
+        })
+        .catch(err => console.log(err));
+};
 
 //Combines the geolocation api and weather api into one. Takes a city as an input and outputs its current weather data as a promise.
 const getCurrentWeatherData = (city) => {
     return getLatLon(city)
         .then(locationData => 
             fetchCurrentWeather(locationData))
-        .catch(err => console.log(err))
-}
+        .catch(err => console.log(err));
+};
+
+const getFiveDayWeatherData = (city) => {
+    return getLatLon(city)
+        .then(locationData =>
+            fetchFiveDayWeather(locationData))
+        .catch(err => console.log(err));
+};
 
 const renderWeatherData = async () => {
     const cityToSearch = document.getElementById('cityToSearch').value;
-    const data = await getCurrentWeatherData(cityToSearch);
-    console.log(data)
-    currentCityEl.textContent = data.name;
-    currentTempEl.textContent = data.main.temp;
-    currentWindEl.textContent = data.wind.speed;
-    currentHumidityEl.textContent = data.main.humidity;
-}
+    const currentWeatherData = await getCurrentWeatherData(cityToSearch);
+    console.log('Current Weather Data', currentWeatherData);
+    currentCityEl.textContent = currentWeatherData.name;
+    currentTempEl.textContent = currentWeatherData.main.temp;
+    currentWindEl.textContent = currentWeatherData.wind.speed;
+    currentHumidityEl.textContent = currentWeatherData.main.humidity;
+    const fiveDayWeatherData = await getFiveDayWeatherData(cityToSearch);
+    console.log('Forecast Data',fiveDayWeatherData);
+    dayOneTempEl.textContent = fiveDayWeatherData.list[3].main.temp;
+    dayOneWindEl.textContent = fiveDayWeatherData.list[3].wind.speed;
+    dayOneHumidityEl.textContent = fiveDayWeatherData.list[3].main.humidity;
+};
 
 renderWeatherData();
 searchBtnEl.addEventListener('click', () => renderWeatherData());
